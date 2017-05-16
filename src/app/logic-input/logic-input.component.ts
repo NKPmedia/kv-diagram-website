692f9f283a26a2d728bf56c92ac3d43f54dfed2a
nkpmedia <webmaster@nkp-media.de>
2017-05-17 01:40:18 +0200

    Removed the base tag. Now a browser will find the js files
    removed /dist from .gitignore

--- src/app/logic-input/logic-input.component.ts
+++ src/app/logic-input/logic-input.component.ts
@@ -17,7 +17,7 @@ export class LogicInputComponent implements OnInit {
   @Input()
   kvDiagramCom: KVDiagramComponent;
 
-  private logicPhraseString = "";
+  logicPhraseString = "";
   private logicPhrase: LogicPhrase;
 
   constructor(private basicLogicPhraseParser: BasicLogicPhraseParser) { }
